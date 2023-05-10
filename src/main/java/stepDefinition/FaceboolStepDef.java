b9f34381273cb3d1ddc519f8d4c40b5b70325770
anurag3263 <80739367+anurag3263@users.noreply.github.com>
2023-05-10 22:05:07 +0530

    Update FaceboolStepDef.java

--- src/main/java/stepDefinition/FaceboolStepDef.java
+++ src/main/java/stepDefinition/FaceboolStepDef.java
@@ -6,7 +6,7 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
